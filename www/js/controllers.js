696cf149303e328f647ac447c9a30cf5b7f826ac
Phil Skaggs <prskaggsjr@gmail.com>
2016-06-30 20:38:08 -0600

    splits on spaces now

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,9 +12,18 @@ angular.module('app.controllers', [])
 
   function addMessageToList(message){
     vm.messages.push({content:$sce.trustAsHtml(message)});
-    botMessageToList();
+    // botMessageToList();
     reviewMessage(message);
   }
+
+  var elizaQuits = [
+    "hi",
+    "yo",
+    "hello",
+    "hey",
+    "what's up",
+    "howdy",
+  ];
 
   function reviewMessage(text) {
     text=text.toLowerCase();
@@ -24,21 +33,51 @@ angular.module('app.controllers', [])
   	text=text.replace(/\s*\bbut\b\s*/g, '.');
   	text=text.replace(/\s{2,}/g, ' ');
 
-    console.log(text);
+    // console.log(text);
 
-    var parts=text.split('.');
+    var parts=text.split('.').join(' ').split(' ');
+    console.log(parts);
     for (var i=0; i<parts.length; i++) {
       var part=parts[i];
+      // console.log(part);
       if (part!='') {
         // check for quit expression
         for (var q=0; q<elizaQuits.length; q++) {
           if (elizaQuits[q]==part) {
-            this.quit=true;
-            return this.getFinal();
+            var hi = getFinal();
+            vm.messages.push({content:(hi.saying), emoji:$sce.trustAsHtml(hi.emoji)});
+            $ionicScrollDelegate.scrollBottom(true)
           }
         }
       }
     }
+  }
+
+  var elizaFinals = [
+    {
+      saying: "Hey.",
+      emoji: "&#128515;"
+    },
+    {
+      saying: "",
+      emoji: "&#128521;"
+    },
+    {
+      saying: "Yo.",
+      emoji: "&#128526;"
+    },
+    {
+      saying: "",
+      emoji: "&#128540;"
+    },
+    {
+      saying: "Hello...",
+      emoji: "&#128524;"
+    },
+  ];
+
+  var getFinal = function() {
+  	return elizaFinals[Math.floor(Math.random()*elizaFinals.length)];
   }
 
 
